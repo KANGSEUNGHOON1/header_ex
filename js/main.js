45793018b3883988c09ebff45410c52f23b8d621
KANGSEUNGHOON <xgde0324@gmail.com>
2025-01-21 13:12:32 +0900

    모바일 메뉴 호버

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
 $(document).ready(function () {
+  // 햄버거바 클릭
   $("#hamburger-menu").click(function () {
-    $("#mobile-nav").animate(
+    $(".mobile-nav").animate(
       {
         right: 0,
       },
       300
     );
   });
-  // 모바일용 닫기 버튼
+  //모바일용 메뉴 클릭시
+  $(".mobile-nav .gnb > li >a").click(function(){
+    $(this).next(".mobile-nav .depth2").stop().slideToggle()
+  })
+  // 모바일용 메뉴 닫기 버튼
   $("#close-menu").click(function () {
-    $("#mobile-nav").animate(
+    $(".mobile-nav").animate(
       {
-        right: "-300px",
+        right: "-300px", //메뉴가 오른쪽 밖으로 나가도록함
       },
       300
     );
   });
+  // 메뉴에 호버했을때
+  $("nav .gnb").hover(function(){
+    // 마우스를 올렸을때 
+    $("nav .depth2 , .header-bg").stop().slideDown()
+  },
+  function(){
+     // 마우스를 땟을때 
+     $("nav .depth2 , .header-bg").stop().slideUp()
+
+  }
+)
+
 });
